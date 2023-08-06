ab60e05082255d406f43b99928a474bf5f2b8a31
Boris Rybalkin <ribalkin@gmail.com>
2023-08-06 09:57:13 +0100

    fix services

--- photoprism/photoprism.sh
+++ photoprism/photoprism.sh
@@ -2,4 +2,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/opt/photoprism/bin/lib)
-${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/opt/photoprism/bin/photoprism "$@"
+${DIR}/lib/*-linux*/ld-*.so.1 --library-path $LIBS ${DIR}/opt/photoprism/bin/photoprism "$@"
