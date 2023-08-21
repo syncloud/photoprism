732b7132568b976408516dec0bc3a8cb22a8637e
Boris Rybalkin <ribalkin@gmail.com>
2023-08-21 17:55:31 +0100

    config path

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-exec ${DIR}/photoprism/bin/photoprism.sh --config-path /var/snap/photoprism/config start
+exec ${DIR}/photoprism/bin/photoprism.sh --config-path /var/snap/photoprism/current/config start
