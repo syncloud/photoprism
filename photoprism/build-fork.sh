9a038134c9baf78d746ec317ab9ff9aabc6c2d48
Boris Rybalkin <ribalkin@gmail.com>
2023-08-23 23:22:43 +0100

    wget

--- photoprism/build-fork.sh
+++ photoprism/build-fork.sh
@@ -2,6 +2,8 @@
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
+apt update
+apt install -y wget
 wget https://github.com/cyberb/photoprism/archive/refs/heads/develop.tar.gz
 tar xf develop.tar.gz
 cd photoprism-develop
