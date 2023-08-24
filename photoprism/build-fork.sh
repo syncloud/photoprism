2f1abf13b151a709f5fb17c1662a95b99f1e48ec
Boris Rybalkin <ribalkin@gmail.com>
2023-08-24 13:11:19 +0100

    non debian

--- photoprism/build-fork.sh
+++ photoprism/build-fork.sh
@@ -2,15 +2,7 @@
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
-export DEBIAN_FRONTEND="noninteractive"
-echo 'APT::Acquire::Retries "3";' > /etc/apt/apt.conf.d/80retries
-echo 'APT::Install-Recommends "false";' > /etc/apt/apt.conf.d/80recommends
-echo 'APT::Install-Suggests "false";' > /etc/apt/apt.conf.d/80suggests
-echo 'APT::Get::Assume-Yes "true";' > /etc/apt/apt.conf.d/80forceyes
-echo 'APT::Get::Fix-Missing "true";' > /etc/apt/apt.conf.d/80fixmissing
-echo 'force-confold' > /etc/dpkg/dpkg.cfg.d/force-confold
-apt-get update
-apt-get -qq install wget git
+#apk add git
 git clone https://github.com/cyberb/photoprism.git
 cd photoprism
 make docker-build
