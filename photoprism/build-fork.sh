4950eb03cba8688f52a5b5409c29e7a958f1086f
Boris Rybalkin <ribalkin@gmail.com>
2024-05-25 13:35:13 +0100

    rebased upstream fix

--- photoprism/build-fork.sh
+++ photoprism/build-fork.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 wget --progress=dot:giga https://github.com/cyberb/photoprism/archive/refs/heads/upstream.tar.gz
-tar xf develop.tar.gz
-cd photoprism-develop
+tar xf upstream.tar.gz
+cd photoprism-upstream
 make dep-tensorflow
 make build-go
