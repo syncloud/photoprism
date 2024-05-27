c289776df39956716744feaa2a4d0b6b36af258e
Boris Rybalkin <ribalkin@gmail.com>
2024-05-27 22:01:57 +0100

    rebased fork

--- photoprism/build-fork.sh
+++ photoprism/build-fork.sh
@@ -2,9 +2,9 @@
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
-wget --progress=dot:giga https://github.com/cyberb/photoprism/archive/refs/heads/upstream.tar.gz
-tar xf upstream.tar.gz
-mv photoprism-upstream photoprism-fork
+wget --progress=dot:giga https://github.com/cyberb/photoprism/archive/refs/heads/develop.tar.gz
+tar xf develop.tar.gz
+mv photoprism-develop photoprism-fork
 cd photoprism-fork
 make dep-tensorflow
 make build-go
