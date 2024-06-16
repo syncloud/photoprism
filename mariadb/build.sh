27228464b2c2f2b2ff4a7e2db6dedb47a13dc9fb
Boris Rybalkin <ribalkin@gmail.com>
2024-06-16 13:55:18 +0100

    fix test

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -7,5 +7,9 @@ BUILD_DIR=${DIR}/../build/snap/mariadb
 mkdir -p ${BUILD_DIR}
 
 cp -r /usr ${BUILD_DIR}/usr
+mv ${BUILD_DIR}/usr/bin/resolveip ${BUILD_DIR}/usr/bin/resolveip.bin
+mv ${BUILD_DIR}/usr/bin/my_print_defaults ${BUILD_DIR}/usr/bin/my_print_defaults.bin
+mv ${BUILD_DIR}/usr/bin/mysqld ${BUILD_DIR}/usr/bin/mysqld.bin
+mv ${BUILD_DIR}/usr/bin/mysql ${BUILD_DIR}/usr/bin/mysql.bin
 cp ${DIR}/bin/* ${BUILD_DIR}/usr/bin
 
