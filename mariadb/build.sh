205059f07494e57b12e02f313d01fac67be92215
Boris Rybalkin <ribalkin@gmail.com>
2024-06-17 06:46:22 +0100

    fix dump

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -13,6 +13,7 @@ mv ${BUILD_DIR}/usr/bin/resolveip ${BUILD_DIR}/usr/bin/resolveip.bin
 mv ${BUILD_DIR}/usr/bin/my_print_defaults ${BUILD_DIR}/usr/bin/my_print_defaults.bin
 mv ${BUILD_DIR}/usr/bin/mysqld ${BUILD_DIR}/usr/bin/mysqld.bin
 mv ${BUILD_DIR}/usr/bin/mysql ${BUILD_DIR}/usr/bin/mysql.bin
+mv ${BUILD_DIR}/usr/bin/mariadb_dump ${BUILD_DIR}/usr/bin/mariadb_dump.bin
 
 cp ${DIR}/bin/* ${BUILD_DIR}/usr/bin
 
