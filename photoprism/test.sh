7573e36ac7ff0104f1d77bf5aec7442a95ef1119
Boris Rybalkin <ribalkin@gmail.com>
2024-07-20 10:50:45 +0100

    test user

--- photoprism/test.sh
+++ photoprism/test.sh
@@ -5,5 +5,10 @@ cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/photoprism
 ${BUILD_DIR}/bin/darktable-cli.sh -v
+
+useradd test
+
+sudo -u test ${BUILD_DIR}/bin/darktable-cli.sh -v
+
 #${BUILD_DIR}/bin/darktable-cli.sh test123.orf test.jpg --apply-custom-presets false --width 7680 --height 7680 --hq true --upscale false --core --library :memory:
 
