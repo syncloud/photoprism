70ef40745dce4a39118b1690dea2a44c4da16d96
Boris Rybalkin <ribalkin@gmail.com>
2024-07-20 13:10:32 +0100

    test config dir

--- photoprism/test.sh
+++ photoprism/test.sh
@@ -7,7 +7,8 @@ BUILD_DIR=${DIR}/../build/snap/photoprism
 #${BUILD_DIR}/bin/darktable-cli.sh -v
 
 useradd test
-sudo -u test ${BUILD_DIR}/bin/darktable-cli.sh -v
+ls -la /home/test
+sudo -u test ${BUILD_DIR}/bin/darktable-cli.sh -v --core --configdir `pwd`
 
 #${BUILD_DIR}/bin/darktable-cli.sh test123.orf test.jpg --apply-custom-presets false --width 7680 --height 7680 --hq true --upscale false --core --library :memory:
 
