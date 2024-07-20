b9afec80741a270e439d7908b4ef458d8b7db2e4
Boris Rybalkin <ribalkin@gmail.com>
2024-07-20 12:31:39 +0100

    include opt

--- photoprism/build.sh
+++ photoprism/build.sh
@@ -10,6 +10,7 @@ cd ${BUILD_DIR}
 cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
+cp -r /opt ${BUILD_DIR}
 #cp -r /lib64 ${BUILD_DIR} || true
 
 cp -r ${DIR}/bin/* ${BUILD_DIR}/bin
