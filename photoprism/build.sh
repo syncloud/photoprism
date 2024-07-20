bfcb38ac8c51c875aadfcce6cd3c925a974ac020
Boris Rybalkin <ribalkin@gmail.com>
2024-07-20 12:25:53 +0100

    fix bin

--- photoprism/build.sh
+++ photoprism/build.sh
@@ -12,8 +12,6 @@ cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
 #cp -r /lib64 ${BUILD_DIR} || true
 
-cp ${DIR}/photoprism.sh ${BUILD_DIR}/bin/
-cp ${DIR}/darktable-cli.sh ${BUILD_DIR}/bin/
-cp ${DIR}/ffmpeg.sh ${BUILD_DIR}/bin/
+cp -r ${DIR}/bin/* ${BUILD_DIR}/bin
 cd ${BUILD_DIR}
 ln -s lib/*-linux*/ld-*.so* ld.so
