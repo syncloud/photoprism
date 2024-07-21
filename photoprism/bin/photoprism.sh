ec4c1e4101a277b7aba52aa7505deeb85ad2cac8
Boris Rybalkin <ribalkin@gmail.com>
2024-07-21 18:25:33 +0100

    fix path again

--- photoprism/bin/photoprism.sh
+++ photoprism/bin/photoprism.sh
@@ -2,5 +2,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/opt/photoprism/lib)
-export PATH=${DIR}/photoprism/bin:$PATH
+export PATH=${DIR}/bin:$PATH
 exec ${DIR}/lib/ld.so --library-path $LIBS ${DIR}/opt/photoprism/bin/photoprism "$@"
