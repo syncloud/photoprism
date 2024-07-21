0b5bc0a2c21df294f3a717d4d771d5a0a4f8cdf1
Boris Rybalkin <ribalkin@gmail.com>
2024-07-21 19:49:14 +0100

    perl fix

--- photoprism/bin/photoprism.sh
+++ photoprism/bin/photoprism.sh
@@ -3,4 +3,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/opt/photoprism/lib)
 export PATH=${DIR}/bin:$PATH
+export PERL5LIB=${DIR}/usr/share/perl5
 exec ${DIR}/lib/ld.so --library-path $LIBS ${DIR}/opt/photoprism/bin/photoprism "$@"
