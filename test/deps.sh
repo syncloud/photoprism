419f57736f5da846b042def27b2d7def9ff77d11
Boris Rybalkin <ribalkin@gmail.com>
2024-07-21 21:12:58 +0100

    wget

--- test/deps.sh
+++ test/deps.sh
@@ -5,6 +5,6 @@ while ! apt-get update; do
   sleep 1
   echo "retry"
 done
-apt-get install -y sshpass openssh-client
+apt-get install -y sshpass openssh-client wget
 pip install -r requirements.txt
 wget https://dl.photoprism.app/samples/Formats/Image/HEIC/20220831_001704_66A1ECB0.heic
