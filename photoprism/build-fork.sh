2ed214c9d25b2d46994c5321ad7a12d417aa0967
Boris Rybalkin <ribalkin@gmail.com>
2026-05-11 08:07:59 +0100

    rename ldap-webdav.patch -> ldap.patch; readable UPSTREAM_TAG check
    The patch hooks entity.Auth which both the web sign-in form and the
    WebDAV basic-auth handler call into; calling it webdav-specific was
    misleading. The required-env-var check is now a plain if-empty so it's
    obvious what runs when UPSTREAM_TAG is missing.

--- photoprism/build-fork.sh
+++ photoprism/build-fork.sh
@@ -3,7 +3,10 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
 BUILD_DIR=${DIR}/../build/snap/photoprism
 
-: "${UPSTREAM_TAG:?required: upstream release tag e.g. 260305-fad9d5395}"
+if [ -z "$UPSTREAM_TAG" ]; then
+  echo "UPSTREAM_TAG is required" >&2
+  exit 1
+fi
 
 cd ${DIR}
 wget --progress=dot:giga https://github.com/photoprism/photoprism/archive/refs/tags/${UPSTREAM_TAG}.tar.gz
@@ -11,7 +14,7 @@ tar xf ${UPSTREAM_TAG}.tar.gz
 mv photoprism-${UPSTREAM_TAG} photoprism-src
 
 cd photoprism-src
-patch -p1 < ${DIR}/ldap-webdav.patch
+patch -p1 < ${DIR}/ldap.patch
 make dep-tensorflow
 make build-go
 
